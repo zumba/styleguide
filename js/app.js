a10706f07dbb75c537a92c41d0b703917ff3bb73
Eric Rainey <eric.rainey@zumba.com>
2015-05-21 11:22:58 -0400

    Add directive to work with Prism library on compiled HTML from subpages

--- js/app.js
+++ js/app.js
@@ -4,69 +4,88 @@ var ZSG = angular.module('ZSG', ['ngRoute']);
 
 /* ContentController */
 ZSG.controller('ContentController', function($scope, $location){
-    /* Sets the sidebar link to 'active' class when on the current page */
-    $scope.setActive = function(path) {
-        console.log($location.path().substr(0, path.length));
-        console.log(path);
-        alert("hello")
-         if ($location.path().substr(0, path.length) == path) {
-            return "active";
-        } else {
-            return "";
-        }
-    };
+	/* Sets the sidebar link to 'active' class when on the current page */
+	$scope.setActive = function(path) {
+		 if ($location.path().substr(0, path.length) == path) {
+			return "active";
+		} else {
+			return "";
+		}
+	};
 });
 
 /* Router */
 ZSG.config(function($routeProvider) {
-    $routeProvider
-    .when('/', 
-    {
-        templateUrl: 'views/intro.html',
-        controller: 'ContentController'
-    })
+	$routeProvider
+	.when('/', 
+	{
+		templateUrl: 'views/intro.html',
+		controller: 'ContentController'
+	})
 
-    .when(
-        '/contents/code', 
-        {
-            templateUrl: 'views/contents/code.html', 
-            controller: 'ContentController'
-        })
+	.when(
+		'/contents/code', 
+		{
+			templateUrl: 'views/contents/code.html', 
+			controller: 'ContentController'
+		})
 
-    .when(
-        '/contents/grid', 
-        {
-            templateUrl: 'views/contents/grid.html', 
-            controller: 'ContentController'
-        })
+	.when(
+		'/contents/grid', 
+		{
+			templateUrl: 'views/contents/grid.html', 
+			controller: 'ContentController'
+		})
 
-    .when(
-        '/contents/breakpoints', 
-        {
-            templateUrl: 'views/contents/breakpoints.html', 
-            controller: 'ContentController'
-        })
+	.when(
+		'/contents/breakpoints', 
+		{
+			templateUrl: 'views/contents/breakpoints.html', 
+			controller: 'ContentController'
+		})
 
-    .when(
-        '/contents/colors', 
-        {
-            templateUrl: 'views/contents/colors.html', 
-            controller: 'ContentController'
-        })
+	.when(
+		'/contents/colors', 
+		{
+			templateUrl: 'views/contents/colors.html', 
+			controller: 'ContentController'
+		})
 
-    .when(
-        '/contents/patterns', 
-        {
-            templateUrl: 'views/contents/patterns.html', 
-            controller: 'ContentController'
-        })
+	.when(
+		'/contents/patterns', 
+		{
+			templateUrl: 'views/contents/patterns.html', 
+			controller: 'ContentController'
+		})
 
-    .when(
-        '/contents/typography', 
-        {
-            templateUrl: 'views/contents/typography.html', 
-            controller: 'ContentController'
-        })
-    
-    .otherwise('/')
+	.when(
+		'/contents/typography', 
+		{
+			templateUrl: 'views/contents/typography.html', 
+			controller: 'ContentController'
+		})
+	
+	.otherwise('/')
 });
+
+
+/*  For making Prism work with compiled HTML in subpages
+**  syntax to add Prism highlighting is:
+**  <pre zsg-prism class="language-markup" source="<h1>"></pre>
+*/
+ZSG.directive('zsgPrism', [function() {
+	return {
+		restrict: 'A',
+		scope: {
+			source: '@'
+		},
+		link: function(scope, element, attrs) {
+			scope.$watch('source', function(v) {
+				if(v) {
+					Prism.highlightElement(element.find("code")[0]);
+				}
+			});
+		},
+		template: "<code ng-bind='source'></code>"
+	};
+}]);
